b946a00e3283b6fac6f2cea0fd3b04a073ed8db3
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2024-06-01 14:15:15 -0300

    Implementado logica para os teste da DepartmentControllerIT

--- src/main/java/com/devsuperior/demo/services/DepartmentService.java
+++ src/main/java/com/devsuperior/demo/services/DepartmentService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.DepartmentDTO;
+import com.devsuperior.demo.entities.Department;
 import com.devsuperior.demo.repositories.DepartmentRepositoru;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DepartmentService {
@@ -10,4 +15,10 @@ public class DepartmentService {
     @Autowired
     private DepartmentRepositoru departmentRepositoru;
 
+    public List<DepartmentDTO> findAll() {
+        List<Department> departments = departmentRepositoru.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        return departments.stream()
+                .map(DepartmentDTO::new)
+                .toList();
+    }
 }
